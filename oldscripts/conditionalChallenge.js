dea01476661ac3e530fa07ae848e5c561e134d86
Christopher Stanley <christopher.stanley@interapthq.com>
2016-09-19 11:31:40 -0400

    Add files via upload

---
+++ oldscripts/conditionalChallenge.js
@@ -0,0 +1,22 @@
+/* Bronze Challenge
+My kinda guy */
+
+//Silver Challenge
+
+var patriotWins = 2;
+var coltWins = 2;
+
+if (patriotWins > coltWins) {
+    console.log("We lost!");
+}
+else if (patriotWins < coltWins) {
+    console.log("We won!")
+}
+else if (patriotWins == coltWins) {
+    console.log("We tie");
+}
+else if (coltWins == 0) {
+    console.log("Fire Grigson.");
+}
+
+
