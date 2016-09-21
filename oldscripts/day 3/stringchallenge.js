41c8a908803965f8fe2ab2c351bd36d647016f65
ChristopherStanley14 <christopherstanley@ChristopherStanleyMBP.local>
2016-09-21 15:49:45 -0400

    Update

---
+++ oldscripts/day 3/stringchallenge.js
@@ -0,0 +1,12 @@
+var firstName = "Christopher";
+var middleName = "Cohen";
+var lastName = "Stanley";
+
+var fullName = firstName + " " + middleName + " " + lastName;
+console.log(fullName);
+
+
+
+var reversed = fullName.split("").reverse().join("");
+console.log(reversed);
+
---
+++ oldscripts/day 3/vars.js
@@ -0,0 +1,41 @@
+var firstName = "Chris";
+var lastName = "Stanley";
+
+var myAge = 24;
+
+console.log(firstName);
+console.log(lastName);
+console.log(myAge);
+
+firstName = "Christopher";
+lastName = "Stan";
+
+console.log(firstName);
+console.log(lastName);
+
+//Adding two numbers together. 
+var sum = 2 + 2; // sum is 4
+sum = sum + 7; // add 7 to sum and I get 11
+console.log(sum);
+
+//single line comment
+/* Multi-line 
+comment */
+
+var bankBalance = 100;
+var deposit = "10";
+
+console.log(bankBalance + deposit);
+console.log(bankBalance + (deposit * 1));
+console.log(bankBalance + (firstName * 1));
+
+var a = 10, b = 17;
+
+console.log(a);
+console.log(b);
+sum = a + b;
+console.log(sum);
+
+b+= a; //same as b = b + a 
+console.log(b);
+console.log(a == b);
